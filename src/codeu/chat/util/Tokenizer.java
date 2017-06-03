001d43b400cc865953c83e3254b37ec39e3d59a9
Jiahui Chen <jiahui.k.chen@gmail.com>
2017-06-03 17:19:23 -0600

    Pulled transaction_log branch from Dita

--- src/codeu/chat/util/Tokenizer.java
+++ src/codeu/chat/util/Tokenizer.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.function.Function;
 
-//comment to test the first commit to the persistent_storage branch
 public final class  Tokenizer implements Iterator<String> {
 
     //stores the final "token" form of the input
